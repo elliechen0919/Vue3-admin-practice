852ed0f5b5e075ee09081912ad62027962cf67b1
newstarellie <newstarellie@gmail.com>
2023-08-07 17:35:26 +0800

    change setting of member\basic.vue and router setting

--- src/router/modules/member.ts
+++ src/router/modules/member.ts
@@ -35,7 +35,7 @@ const routes: Array<RouteRecordRaw> = [
         meta: {
           title: 'Member Management',
         },
-        component: () => import('@/views/comp/table/basic.vue'),
+        component: () => import('@/views/member/basic.vue'),
       },
       {
         path: 'member-basic-info/:id?',
